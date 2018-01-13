d0a14380928a6225289374bbbe569c8efe8f9bc0
Mykhailo Zhyliakov <fizarum@gmail.com>
2018-01-13 23:27:53 +0100

    added rotation for 270 degrees; added support for codeblocks;

--- main.cpp
+++ main.cpp
@@ -14,13 +14,33 @@ typedef unsigned char byte;
 typedef byte * bytePtr;
 
 
-void printArray(bytePtr array, size_t arraySize) {
+void printArray(bytePtr array, size_t arraySize, bool printAdress = false) {
     std::string str;
-    printf("%p: \n", (void*)array);
+    if(printAdress) {
+        printf(" %p: ", (void*)array);
+    }
+    printf("\n");
     for(size_t i = 0; i < arraySize; i++) {
         str += ' ' + std::to_string(array[i]);
     }
     printf("%s\n", str.c_str());
+}
+
+void printArray2D(bytePtr array, size_t arraySize, size_t width, bool printAdress = false) {
+    std::string str;
+    if(printAdress) {
+        printf(" %p: \n", (void*)array);
+    }
+    printf("\n");
+    size_t rows = arraySize / width;
+
+    for(size_t i = 0; i < rows; ++i) {
+        for(size_t index = 0; index < width; ++index) {
+           str += ' ' + std::to_string(array[i * width + index]);
+        }
+        printf("%s\n", str.c_str());
+        str = "";
+    }
 }
 
 /**
@@ -35,6 +55,25 @@ bytePtr rotate90cw(bytePtr data, size_t length, size_t width) {
     size_t incr = 0;
     for(long long int shift = width; shift >= 0; --shift) {
         for(long long int index = length - shift; index >= 0 && incr < length; index -= width) {
+            result[incr] = data[index];
+            incr++;
+        }
+    }
+    return result;
+}
+
+/**
+ * rotate image on 90 deg (ccw direction) or 270 deg
+ * @param data - original image to rotate
+ * @param length - original image length, in pixels (bytes)
+ * @param width - image width
+ * @return rotated image
+ * */
+bytePtr rotate270cw(bytePtr data, size_t length, size_t width) {
+    bytePtr result = new byte[length];
+    size_t incr = 0;
+    for(long long int shift = width - 1; shift >= 0; --shift) {
+        for(long long int index = shift; index <= length && incr < length; index += width) {
             result[incr] = data[index];
             incr++;
         }
@@ -61,18 +100,30 @@ int main() {
     //2d array = 3 x 4
     size_t arraySize = 12;
     size_t width = 3;
+    size_t height = arraySize / width;
 
     bytePtr test = new byte[arraySize];
     for(size_t i = 0; i < arraySize; i++) {
         test[i] = i * 2;
     }
 
-    bytePtr result = rotate90cw(test, arraySize, width);
+    printf("\noriginal 2d (width :%d) array: ", width);
+    printArray2D(test, arraySize, width);
+
+    printf("\nflat view: ", width);
     printArray(test, arraySize);
+
+    printf("\n90 deg test: ");
+    bytePtr result = rotate90cw(test, arraySize, width);
     printArray(result, arraySize);
-    printf("\n180 deg test\n");
+
+    printf("\n180 deg test: ");
     result = rotate180(test, arraySize);
     printArray(result, arraySize);
 
+    printf("\n270 deg test: ");
+    result = rotate270cw(test, arraySize, width);
+    printArray(result, arraySize);
+    printArray2D(result, arraySize, height);
     return 0;
 }
